a86e8bc98ec19dcd580ccbe569eca3d9fe4d9450
TW Yang <twy@30abysses.com>
2016-11-15 11:49:55 -0800

    Implement update-metacontents Program.cs

--- 30abysses-www-utilities/src/update-metacontents/Program.cs
+++ 30abysses-www-utilities/src/update-metacontents/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using _30abysses.WWW.Utilities.Common.RawContents.Contents;
+using _30abysses.WWW.Utilities.Common.RawContents.Interfaces;
+using System;
+using System.IO;
 
 namespace _30abysses.WWW.Utilities.UpdateMetaContents
 {
@@ -9,7 +9,53 @@ namespace _30abysses.WWW.Utilities.UpdateMetaContents
     {
         public static int Main(string[] commandLineArguments)
         {
-            return 1;
+            string inputDirectoryPath;
+            string outputDirectoryPath;
+            try
+            {
+                if (commandLineArguments == null)
+                { throw new ArgumentNullException(nameof(commandLineArguments)); }
+
+                {
+                    const int expectedArgumentCount = 2;
+                    var actualArgumentCount = commandLineArguments.Length;
+                    if (actualArgumentCount != expectedArgumentCount)
+                    { throw new ArgumentException($"Received {actualArgumentCount} argument(s) but expected {expectedArgumentCount} argument(s).", nameof(commandLineArguments)); }
+                }
+
+                const string nullOrWhiteSpaceArgumentExceptionMessage = "String argument `{0}` must not be null, empty, or consist only of white-space characters.";
+                {
+                    var commandLineArgument0 = commandLineArguments[0];
+                    const string argumentName = nameof(commandLineArgument0);
+                    if (string.IsNullOrWhiteSpace(commandLineArgument0))
+                    { throw new ArgumentException(string.Format(nullOrWhiteSpaceArgumentExceptionMessage, argumentName), argumentName); }
+                    if (!Directory.Exists(commandLineArgument0))
+                    { throw new ArgumentException($"Input directory `{commandLineArgument0}` does not exist.", argumentName); }
+                    inputDirectoryPath = Path.GetFullPath(commandLineArgument0);
+                    Console.WriteLine($"Converted {argumentName} `{commandLineArgument0}` to {nameof(inputDirectoryPath)} `{inputDirectoryPath}`.");
+                }
+
+                {
+                    var commandLineArgument1 = commandLineArguments[1];
+                    const string argumentName = nameof(commandLineArgument1);
+                    if (string.IsNullOrWhiteSpace(commandLineArgument1))
+                    { throw new ArgumentException(string.Format(nullOrWhiteSpaceArgumentExceptionMessage, argumentName), argumentName); }
+                    if (File.Exists(commandLineArgument1))
+                    { throw new ArgumentException($"Output directory `{commandLineArgument1}` is occupied by a file."); }
+                    outputDirectoryPath = Path.GetFullPath(commandLineArgument1);
+                    Console.WriteLine($"Converted {argumentName} `{commandLineArgument1}` to {nameof(outputDirectoryPath)} `{outputDirectoryPath}`.");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return 1;
+            }
+
+            var contentsRoot = new ContentsRoot(inputDirectoryPath);
+            var updateMetaContentsVisitor = new UpdateMetaContentsVisitor(contentsRoot.Path, outputDirectoryPath);
+            ((IVisitable) contentsRoot).Accept(updateMetaContentsVisitor);
+            return 0;
         }
     }
 }
---
+++ 30abysses-www-utilities/src/update-metacontents/UpdateMetaContentsVisitor.cs
@@ -0,0 +1,20 @@
+﻿using _30abysses.WWW.Utilities.Common.RawContents.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _30abysses.WWW.Utilities.UpdateMetaContents
+{
+    public class UpdateMetaContentsVisitor : ContentVisitor
+    {
+        private string outputDirectoryPath;
+        private string path;
+
+        public UpdateMetaContentsVisitor(string path, string outputDirectoryPath)
+        {
+            this.path = path;
+            this.outputDirectoryPath = outputDirectoryPath;
+        }
+    }
+}
