f5bf99c2f31686b9d7e29ab3f00784eed0a9b870
TW Yang <twy@30abysses.com>
2016-11-15 01:26:52 -0800

    Set up ContentsRoot

--- 30abysses-www-utilities/src/common/RawContents/Contents/ContentsRoot.cs
+++ 30abysses-www-utilities/src/common/RawContents/Contents/ContentsRoot.cs
@@ -5,7 +5,17 @@ using System.Threading.Tasks;
 
 namespace _30abysses.WWW.Utilities.Common.RawContents.Contents
 {
-    public class ContentsRoot
+    public class ContentsRoot : Container, IVisitable
     {
+        public WwwRoot WwwRoot { get; }
+
+        public ContentsRoot(string path) : base(path, null) { WwwRoot = WwwRoot.Get(this); }
+
+        void IVisitable.Accept(ContentsVisitor visitor)
+        {
+            visitor.Visit(this);
+            ((IVisitable) WwwRoot).Accept(visitor);
+            visitor.Leave(this);
+        }
     }
 }
