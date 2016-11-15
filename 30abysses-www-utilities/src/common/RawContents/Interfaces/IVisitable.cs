abf1334cab7e42e36e3fd97cfa7986b29b5f9db7
TW Yang <twy@30abysses.com>
2016-11-15 05:30:30 -0800

    Clean up

--- 30abysses-www-utilities/src/common/RawContents/Interfaces/IVisitable.cs
+++ 30abysses-www-utilities/src/common/RawContents/Interfaces/IVisitable.cs
@@ -1,8 +1,4 @@
 ﻿using _30abysses.WWW.Utilities.Common.RawContents.Abstracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace _30abysses.WWW.Utilities.Common.RawContents.Interfaces
 {
