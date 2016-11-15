b0ab63c8ce8d9f7dfaa2dd616372dd62a96ed7a5
TW Yang <twy@30abysses.com>
2016-11-15 12:16:44 -0800

    Add ContentIO.cs

---
+++ 30abysses-www-utilities/src/common/ContentIO.cs
@@ -0,0 +1,14 @@
+﻿namespace _30abysses.WWW.Utilities.Common
+{
+    public class ContentIO
+    {
+        public string RootInputDirectoryPath { get; }
+        public string RootOutputDirectoryPath { get; }
+
+        public ContentIO(string rootInputDirectoryPath, string rootOutputDirectoryPath)
+        {
+            RootInputDirectoryPath = rootInputDirectoryPath;
+            RootOutputDirectoryPath = rootOutputDirectoryPath;
+        }
+    }
+}
