c848f93cf74a15cdbd4c5f788cf3307ddb0be67e
TW Yang <twy@30abysses.com>
2016-11-15 04:00:00 -0800

    Add Topic

---
+++ 30abysses-www-utilities/src/common/RawContents/Contents/Topic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _30abysses.WWW.Utilities.Common.RawContents.Contents
+{
+    public class Topic
+    {
+        internal static IEnumerable<Topic> Get(Day day)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
